ee50b51f4991133d51705f027fe15e52473767f3
tmlerregcoder <lerreg22@gmail.com>
2022-07-14 19:37:37 -0300

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule , MatDatepickerModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,15 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<p>
+  <mat-form-field appearance="fill">
+    <mat-label>Input disabled</mat-label>
+    <input matInput [matDatepicker]="dp3" disabled>
+    <mat-hint>MM/DD/YYYY</mat-hint>
+    <mat-datepicker-toggle matSuffix [for]="dp3"></mat-datepicker-toggle>
+    <mat-datepicker #dp3 disabled="false"></mat-datepicker>
+  </mat-form-field>
+</p>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
