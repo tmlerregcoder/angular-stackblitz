7f06e005d91dcda98e3490c7e945a7b80d947776
tmlerregcoder <lerreg22@gmail.com>
2022-07-14 19:47:11 -0300

    3 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , MatDatepickerModule],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,15 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<p>
-  <mat-form-field appearance="fill">
-    <mat-label>Input disabled</mat-label>
-    <input matInput [matDatepicker]="dp3" disabled>
-    <mat-hint>MM/DD/YYYY</mat-hint>
-    <mat-datepicker-toggle matSuffix [for]="dp3"></mat-datepicker-toggle>
-    <mat-datepicker #dp3 disabled="false"></mat-datepicker>
-  </mat-form-field>
-</p>`,
+  templateUrl: './hello.component.html',
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
